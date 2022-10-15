04d9903ff23e1acf5ddca6b0819d6d4ad46edfcb
sowjanyagarapati <46521587+sowjanyagarapati@users.noreply.github.com>
2022-10-15 07:59:45 +0530

    Update utils.py

--- fast_api/utils.py
+++ fast_api/utils.py
@@ -4,10 +4,20 @@ import pandas as pd
 
 
 class commonUtils():
+    """
+    Contains common functions required for the project
+    """
     def __init__(self):
         self.columns = ["txn","acc_number","rrn","ifsc_number","bank_name","acc_holder_name","txn_type"]
 
     def clean_df(self,df):
+        """
+        Cleans the DataFrame as per the requirements.
+        Params:
+        df: DataFrame
+        Return:
+        df: DataFrame after processing
+        """
         narration_list = df["NARRATION"].apply(lambda x: x.split("/")).tolist()
         df2 = pd.DataFrame(narration_list, columns=self.columns)
         df2["rrn"] = df2["rrn"].apply(lambda x: x.split(":")[1])
