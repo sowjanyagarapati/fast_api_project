98e9b34ab0aa877707d46c8114d8bd5f203e3b06
sowjanyagarapati <46521587+sowjanyagarapati@users.noreply.github.com>
2022-10-15 07:58:39 +0530

    Update db.py

--- fast_api/db.py
+++ fast_api/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
+#Crating Database Connection
 sqlite_db_url = "sqlite:///./customers.db"
 engine = create_engine(sqlite_db_url, connect_args={"check_same_thread": False})
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
